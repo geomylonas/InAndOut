056859a25d04a4a203c3263fa6798380a3ca1d8c
George Mylonas <geomylonas@gmail.com>
2022-02-21 10:43:03 +0200

    Add project files.

---
+++ InAndOut/Controllers/AppointmentController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace InAndOut.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+            //return Ok(DateTime.Now.ToShortDateString());
+        }
+
+        public IActionResult Details(int id)
+        {
+            return Ok("You entered id = " + id);
+        }
+    }
+}
